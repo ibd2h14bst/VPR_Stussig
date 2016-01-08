1babee103e199948bd2c258e93ec8d3a31b88224
ibd2h14bst <cornelia.stussig@gmx.de>
2016-01-08 18:49:31 +0100

    Methoden get count delet update hinzugefügt

--- src/db/DatabaseSQLite.java
+++ src/db/DatabaseSQLite.java
@@ -2,11 +2,17 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * 
+ * @author Cornelia Stussig
+ *
+ */
 public class DatabaseSQLite implements IDatabase {
 
 	private static final String DB_DRIVER = "org.sqlite.JDBC";
@@ -51,30 +57,119 @@ public class DatabaseSQLite implements IDatabase {
 		}
 	}
 	
-	private void execute(String sql) throws SQLException {
+	private ResultSet execute(String sql) throws SQLException {
 		Statement stmt = connection.createStatement();
 		
-		stmt.addBatch(sql);
-		stmt.executeBatch();
+		ResultSet resultSet = stmt.executeQuery(sql);
 		
 		stmt.close();
-	}
-
-	public ArrayList<HashMap<String, Object>> get(String tableName, HashMap<String,Object> fields) {
-		// SELECT * FROM student WHERE Klasse = 'ibd2h14b';
-
-		return null;
+		
+		return resultSet;
 	}
 
 	@Override
-	public void delete(String tableName, Object primaryKey) {
+	public ArrayList<HashMap<String, Object>> get(String tableName) {
+		String sql = "SELECT * From " + tableName;
+		ArrayList<HashMap<String, Object>> result = internalGet(sql);
 		
-	}
-
-	@Override
-	public void update(String tableName, Object primaryKey, HashMap<String, Object> fields) {
-		// TODO Auto-generated method stub
-		
+		return result;
 	}
 	
+	@Override
+	public ArrayList<HashMap<String, Object>> get(String tableName, String where ) {
+		String sql = "SELECT * From " + tableName + 
+						" WHERE " + where;
+		ArrayList<HashMap<String, Object>> result = internalGet(sql);
+		
+		return result;
+	}
+	
+	@Override
+	public ArrayList<HashMap<String, Object>> get(String tableName, String key, String operator, String value ) {
+		String sql = "SELECT * From " + tableName + 
+						" WHERE " + key + " " + operator + " " + value;
+		ArrayList<HashMap<String, Object>> result = internalGet(sql);
+		
+		return result;
+	}
+	
+	@Override
+	public ArrayList<HashMap<String, Object>> count(String tableName) {
+		String sql = "SELECT COUNT (*) From " + tableName;
+		ArrayList<HashMap<String, Object>> result = internalGet(sql);
+		
+		return result;
+	}
+	
+	@Override
+	public ArrayList<HashMap<String, Object>> count(String tableName, String where ) {
+		String sql = "SELECT COUNT (*) From " + tableName + 
+						" WHERE " + where;
+		ArrayList<HashMap<String, Object>> result = internalGet(sql);
+		
+		return result;
+	}
+	
+	@Override
+	public ArrayList<HashMap<String, Object>> count(String tableName, String key, String operator, String value ) {
+		String sql = "SELECT COUNT (*) From " + tableName + 
+						" WHERE " + key + " " + operator + " " + value;
+		ArrayList<HashMap<String, Object>> result = internalGet(sql);
+		
+		return result;
+	}
+	private ArrayList<HashMap<String, Object>> internalGet(String sql) {
+		ArrayList<HashMap<String, Object>> result = new ArrayList<HashMap<String, Object>>();
+		try {
+			ResultSet resultSet = execute(sql);
+			while(resultSet.next()){
+				HashMap<String, Object> entry = new HashMap<String, Object>();
+				for (int i = 0; i < resultSet.getMetaData().getColumnCount(); i++) {
+					String columnLabel = resultSet.getMetaData().getColumnLabel(i);
+					Object returnvalue = resultSet.getObject(columnLabel);
+					entry.put(columnLabel, returnvalue);
+				}
+				result.add(entry);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public void delete(String tableName, String where) {
+		String sql = "DELETE From " + tableName + 
+				" WHERE " + where;
+		try {
+			execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void delete(String tableName, String key, String operator, String value ) {
+		String sql = "DELETE From " + tableName + 
+				" WHERE " + key + " " + operator + " " + value;
+		try {
+			execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void update(String tableName, Object where, String key, String value) {
+		String sql = "UPDATE " + tableName +
+					" SET " + key +
+					" = " + value+
+					" WHERE " + where;
+		try {
+			execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+	}	
 }
