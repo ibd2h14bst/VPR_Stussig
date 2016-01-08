5447c2702d1dcf90045ae5512b99b8c28b3ad7a0
ibd2h14bst <cornelia.stussig@gmx.de>
2016-01-08 18:49:45 +0100

    Methoden get count delet update hinzugefügt

--- src/db/IDatabase.java
+++ src/db/IDatabase.java
@@ -5,7 +5,17 @@ import java.util.HashMap;
 
 public interface IDatabase {
 	public void insert(String tableName, HashMap<String, Object> fields);
-	public ArrayList<HashMap<String, Object>> get(String tableName, HashMap<String,Object> fields);
-	public void delete(String tableName, Object primaryKey);
-	public void update(String tableName, Object primaryKey, HashMap<String, Object> fields);
+	
+	public ArrayList<HashMap<String, Object>> get(String tableName);
+	public ArrayList<HashMap<String, Object>> get(String tableName, String where);
+	public ArrayList<HashMap<String, Object>> get(String tableName, String key, String operator, String value);
+
+	public ArrayList<HashMap<String, Object>> count(String tableName);
+	public ArrayList<HashMap<String, Object>> count(String tableName, String where);
+	public ArrayList<HashMap<String, Object>> count(String tableName, String key, String operator, String value);
+	
+	public void delete(String tableName, String where);
+	public void delete(String tableName, String key, String operator, String value);
+	
+	public void update(String tableName, Object where, String key, String value);
 }
